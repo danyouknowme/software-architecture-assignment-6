42e9a6f32608fcba7b957b84ccfe53ba26d91b76
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2023-01-21 19:01:46 +0700

    added requirements

--- recommendations/src/index.js
+++ recommendations/src/index.js
@@ -59,6 +59,8 @@ function setupHandlers(app, db, messageChannel) {
         console.log(JSON.stringify(parsedMsg, null, 4)); // JUST PRINTING THE RECEIVED MESSAGE.
 
         // ... ADD YOUR CODE HERE TO PROCESS THE MESSAGE ...
+        console.log(parsedMsg)
+        console.log(`recommendation video ${parsedMsg.id}`)
 
         console.log("Acknowledging message was handled.");
 
--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -32,10 +32,10 @@ function connectRabbit() {
 //
 // Send the "viewed" to the history microservice.
 //
-function sendViewedMessage(messageChannel, videoPath) {
+function sendViewedMessage(messageChannel, videoPath, id = 0) {
     console.log(`Publishing message on "viewed" exchange.`);
         
-    const msg = { videoPath: videoPath };
+    const msg = { videoPath, id };
     const jsonMsg = JSON.stringify(msg);
     messageChannel.publish("viewed", "", Buffer.from(jsonMsg)); // Publish message to the "viewed" exchange.
 }
@@ -45,8 +45,14 @@ function sendViewedMessage(messageChannel, videoPath) {
 //
 function setupHandlers(app, messageChannel) {
     app.get("/video", (req, res) => { // Route for streaming video.
-
-        const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
+        const videoId = req.query.id;
+        let videoPath;
+        if (videoId == 2) {
+            videoPath = "./videos/bang_to.mp4";
+        } else {
+            videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
+        }
+        
         fs.stat(videoPath, (err, stats) => {
             if (err) {
                 console.error("An error occurred ");
@@ -61,7 +67,7 @@ function setupHandlers(app, messageChannel) {
     
             fs.createReadStream(videoPath).pipe(res);
 
-            sendViewedMessage(messageChannel, videoPath); // Send message to "history" microservice that this video has been "viewed".
+            sendViewedMessage(messageChannel, videoPath, videoId); // Send message to "history" microservice that this video has been "viewed".
         });
     });
 }
